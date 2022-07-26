480d4178fcf7efb2d0b323346445f5d23d7b2d08
Shivam Kumar <shivambharadwaj822@gmail.com>
2022-07-26 12:19:11 +0530

    Update Gmail Validator.py

--- Gmail Validator.py
+++ Gmail Validator.py
@@ -1,21 +1,21 @@
 gmail = input("Enter the Email : ")
 k,j,d = 0,0,0
-if len(gmail)>=6:
-    if gmail[0].isalpha():
-        if ("@" in gmail) and (gmail.count("@")==1):
-            if (gmail[-4]==".") ^ (gmail[-3]=="."):
+if len(gmail)>=6: #condition first
+    if gmail[0].isalpha(): #condition second
+        if ("@" in gmail) and (gmail.count("@")==1): #condition third
+            if (gmail[-4]==".") ^ (gmail[-3]=="."): #condition fourth
                 for i in gmail:
-                    if i==i.isspace():
+                    if i==i.isspace(): #if present then wrong gmail format
                         k = 1
-                    elif i.isalpha():
-                       if i==i.upper():
+                    elif i.isalpha(): #condition fifth
+                       if i==i.upper(): #if present wrong gmail
                         j = 1
-                    elif i.isdigit():
+                    elif i.isdigit(): #condition sixth
                         continue
-                    elif i=="_" or i=="@" or i==".":
+                    elif i=="_" or i=="@" or i==".": #only these symbols to be present inside gmail
                         continue
                     else:
-                        d=1
+                        d=1 #other symbol than wrong gmail
 
                 if k==1 or j==1 or d==1:
                     print("Wrong Gmail 5!")
